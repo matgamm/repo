1ca87193eaadebe0bf1900b37c36ac50d9c32c26
matgamm <mateuszek1088@gmail.com>
2018-12-13 19:53:45 +0100

    Dodałem plik z moim imieniem

--- pierwszy.py
+++ pierwszy.py
@@ -2,9 +2,14 @@
 
 # To jest naszy pierszy skrypt Pythona
 
-print("Witaj swiecie. To jest nasz pierszy sktypt Pythona dzisiaj")
+print("Witaj swiecie. To jest nasz pierszy sktypt Pythona dzisiaj.")
 
-print("Dzis jest 11 grudnia i wieczorem uczymy się Gita")
+print("Dzis jest 11 grudnia i wieczorem uczymy się Gita.")
+
+print("Podaj swoje imię byśmy mogli się zapoznać.")
+nazwa = input()
+print("Witaj ", nazwa, "! Miło Cię poznać.")
+
 
 # to drugi komentarz
 
